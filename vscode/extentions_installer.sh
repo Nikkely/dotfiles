fd113b22b18d7e0a5b75b69f50eabd3a54baa6a5
Nikkely <umivudau153@gmail.com>
2021-04-25 19:30:35 +0900

    vscodeにgit graph追加したので反映

--- vscode/extentions_installer.sh
+++ vscode/extentions_installer.sh
@@ -1,8 +1,10 @@
 # !/bin/sh
 
 # 事前にcodeのパスを通しておけ
+# これで出してる $code --list-extensions | xargs -L 1 echo code --install-extension
 code --install-extension vscodevim.vim
 code --install-extension CoenraadS.bracket-pair-colorizer-2
 code --install-extension dbaeumer.vscode-eslint
 code --install-extension esbenp.prettier-vscode
 code --install-extension Gruntfuggly.todo-tree
+code --install-extension mhutchie.git-graph
